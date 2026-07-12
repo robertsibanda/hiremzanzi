4f7a2cbc63708925f747969b3926906297deb505
Robert sibanda <ROBERTSIBANDA20@GMAIL.COM>
2026-07-12 16:53:52 +0200

    Add files via upload
    first init

---
+++ hiremzanzi/hiremzanzi/src/main/java/com/dev_robert/hiremzanzi/HiremzanziApplication.java
@@ -0,0 +1,13 @@
+package com.dev_robert.hiremzanzi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiremzanziApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HiremzanziApplication.class, args);
+	}
+
+}
---
+++ hiremzanzi/hiremzanzi/src/test/java/com/dev_robert/hiremzanzi/HiremzanziApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev_robert.hiremzanzi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HiremzanziApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
